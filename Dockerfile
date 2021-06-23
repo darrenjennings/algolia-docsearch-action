e3b8c5540e90a964b655be6ca73c1ee1aeba2719
Aditya Mitra <55396651+aditya-mitra@users.noreply.github.com>
2021-06-24 01:53:21 +0530

    refactor(perf): make the action faster (#3)
    ## Summary
    
    Instead of using the docker image by algolia for docsearch, this pr uses the source repository for scrapping and uploading to algolia.
    
    ## Details
    
    Using the source repository, removes the use of jq, installation of docker-cli, the algolia docsearch docker image, and other peer dependencies.
    
    Using the python:3.6 as the base image (which comes with git preinstalled), first the algolia-docsearch repository is git cloned.
    pipenv is installed and then pipenv installed the packages in the Pipfile.
    
    ## Improvements
    
    The running time of the action has now reduced by 40 seconds.
    
    ## Further Comments
    
    I have made a few other fixes/corrections like correcting the spelling of algolia.
    
    Also, I changed the config.example.json since it took a lot of time to index and the difference could not have been made clear.
    
    Closes #2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:latest
+FROM python:3.6
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,23 @@ APPLICATION_ID=$1
 API_KEY=$2
 FILE=$3
 
-apt update
-apt install jq -y
+# build from the main source repository
+git clone https://github.com/algolia/docsearch-scraper.git
 
-# install docker
-apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update
-apt-cache policy docker-ce
-apt install docker-ce -y
+cd docsearch-scraper/
 
-ls -la $GITHUB_WORKSPACE
-cat $GITHUB_WORKSPACE/$FILE | jq -r tostring
-docker run -e APPLICATION_ID=$APPLICATION_ID -e API_KEY=$API_KEY -e "CONFIG=$(cat $GITHUB_WORKSPACE/$FILE | jq -r tostring)" algolia/docsearch-scraper
+# install pipenv without cache
+pip install --no-cache-dir --trusted-host pypi.python.org pipenv
+
+# install packages without virtualenv
+pipenv install --system --deploy --ignore-pipfile
+
+# create the .env file for docsearch
+echo "APPLICATION_ID=${APPLICATION_ID}
+API_KEY=${API_KEY}
+" > .env
+
+# run algolia docsearch
+python docsearch run $GITHUB_WORKSPACE/$FILE
+
+echo "🚀 Successfully indexed and uploaded the results to Algolia"
