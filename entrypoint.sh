499771b706ee581ffae2589478b83ad4831265e9
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-06-23 14:51:19 +0530

    trying making it even faster

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,13 @@ APPLICATION_ID=$1
 API_KEY=$2
 FILE=$3
 
-apt update && apt install git -y
-
 git clone https://github.com/algolia/docsearch-scraper.git
 
 cd docsearch-scraper/
 
-pip install pipenv
+pip install --no-cache-dir --trusted-host pypi.python.org pipenv
 
-pipenv install --system --ignore-pipfile
+pipenv install --system --deploy --ignore-pipfile
 
 echo "APPLICATION_ID=${APPLICATION_ID}
 API_KEY=${API_KEY}
